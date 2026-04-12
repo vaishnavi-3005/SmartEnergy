ea7c219644b0ad81b65ff62b56260f881e480ef4
vaishnavi-3005 <jyotishilavant@gmail.com>
2026-04-12 22:47:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,45 @@
-import gradio as gr
+from fastapi import FastAPI
+from pydantic import BaseModel
+import random
 
-def greet(name):
-    return f"Hello {name}!"
+app = FastAPI()
 
-gr.Interface(fn=greet, inputs="text", outputs="text").launch()
+state = {
+    "step": 0,
+    "energy": 50
+}
+
+class Action(BaseModel):
+    action: int
+
+@app.post("/reset")
+def reset():
+    global state
+    state = {"step": 0, "energy": 50}
+
+    return {
+        "observation": [state["energy"], state["step"]],
+        "reward": 0,
+        "done": False
+    }
+
+@app.post("/step")
+def step(action: Action):
+    global state
+
+    state["step"] += 1
+
+    if action.action == 1:
+        state["energy"] += random.randint(-5, 5)
+        reward = 1
+    else:
+        state["energy"] -= random.randint(0, 3)
+        reward = 0
+
+    done = state["step"] >= 10
+
+    return {
+        "observation": [state["energy"], state["step"]],
+        "reward": reward,
+        "done": done
+    }
